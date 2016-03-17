94a81c5f091a78430709797c544630d176f119da
xala3pa <alvaro.salazar@ticketbis.com>
2016-03-17 20:44:06 +0100

    test with 100% coverage added

--- java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
+++ java/trip-service-kata/src/main/java/org/craftedsw/tripservicekata/trip/TripService.java
@@ -1,17 +1,17 @@
 package org.craftedsw.tripservicekata.trip;
 
-import java.util.ArrayList;
-import java.util.List;
-
 import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
 import org.craftedsw.tripservicekata.user.User;
 import org.craftedsw.tripservicekata.user.UserSession;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class TripService {
 
 	public List<Trip> getTripsByUser(User user) throws UserNotLoggedInException {
 		List<Trip> tripList = new ArrayList<Trip>();
-		User loggedUser = UserSession.getInstance().getLoggedUser();
+		User loggedUser = getLoggedInUser();
 		boolean isFriend = false;
 		if (loggedUser != null) {
 			for (User friend : user.getFriends()) {
@@ -21,12 +21,20 @@ public class TripService {
 				}
 			}
 			if (isFriend) {
-				tripList = TripDAO.findTripsByUser(user);
+				tripList = findTripsBy(user);
 			}
 			return tripList;
 		} else {
 			throw new UserNotLoggedInException();
 		}
 	}
-	
+
+	protected List<Trip> findTripsBy(User user) {
+		return TripDAO.findTripsByUser(user);
+	}
+
+	protected User getLoggedInUser() {
+		return UserSession.getInstance().getLoggedUser();
+	}
+
 }
--- java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
+++ java/trip-service-kata/src/test/java/org/craftedsw/tripservicekata/trip/TripServiceTest.java
@@ -1,5 +1,74 @@
 package org.craftedsw.tripservicekata.trip;
 
+import org.craftedsw.tripservicekata.exception.UserNotLoggedInException;
+import org.craftedsw.tripservicekata.user.User;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.List;
+
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
 public class TripServiceTest {
-	
+
+	public static final User GUEST_USER = null;
+	public static final User UNSED_USER = null;
+	private static final User OTHER_USER = new User();
+	private static final Trip TO_SPAIN = new Trip();
+	private static final Trip TO_LONDON = new Trip();
+	private User loggedInUser;
+	private User REGISTERED_USER = new User();
+
+	private TripService tripService;
+
+
+	@Before
+	public void initialise() {
+		tripService = new TestableTripService();
+
+	}
+
+	@Test(expected = UserNotLoggedInException.class) public void
+    should_throw_an_exception_when_user_is_not_logged_in() {
+		loggedInUser = GUEST_USER;
+
+		tripService.getTripsByUser(UNSED_USER);
+		loggedInUser = REGISTERED_USER;
+	}
+
+
+	@Test public void
+	should_return_friends_trips_when_user_are_friends() {
+		User friend = new User();
+		friend.addFriend(loggedInUser);
+		friend.addTrip(TO_SPAIN);
+		friend.addTrip(TO_LONDON);
+
+		List<Trip> friendTrips  = tripService.getTripsByUser(friend);
+
+	    assertThat(friendTrips.size(), is(2));
+	}
+
+	@Test public void
+	should_not_return_any_trips_when_users_are_not_friends() {
+		User friend = new User();
+		friend.addFriend(OTHER_USER);
+		friend.addTrip(TO_SPAIN);
+
+		List<Trip> friendTrips  = tripService.getTripsByUser(friend);
+
+	}
+
+	private class TestableTripService extends TripService {
+		@Override
+		protected User getLoggedInUser() {
+			return loggedInUser;
+		}
+
+		@Override
+		protected List<Trip> findTripsBy(User user) {
+			return user.trips();
+		}
+	}
 }
